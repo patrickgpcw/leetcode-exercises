48b03d8abff449bb014de7b1c4869c100a6449ab
Patrick Pessanha <patrick.surfista@hotmail.com>
2024-09-26 00:14:33 -0300

    refactor: improving code

--- Easy/IsAnagram/solution.js
+++ Easy/IsAnagram/solution.js
@@ -5,28 +5,29 @@ class Solution {
      *  Primeiro passo - Faço a comparação do tamanho das duas strings passadas como parametro
      *      da função, caso o tamanho seja diferente, não são anagramas.
      * 
-     *  Segundo passo - Crio duas constantes para armazenar o valor e chave de cada char,
+     *  Segundo passo - Crio dois objetos para armazenar o valor e chave de cada char,
      *      o valor seria o char e a chave seria a quantidade de vezes que aquele caracter
      *      se repetiu na palavra .
      *  
-     *  Terceiro passo - Faço um loop para popular as variaveis criadas anteriomente, conforme
+     *  Terceiro passo - Faço um loop para popular os objetos criadas anteriomente, conforme
      *      o valor vai se repetindo, é incrementado +1 na chave.  
      * 
      *  Quarto passo - Faço mais um loop agora para verificar se existe uma diferença entre 
-     *      os hashmaps criados, se não apresentar nenhuma diferença a função retorna verdadeiro. 
+     *      os objetos criados, se não apresentar nenhuma diferença a função retorna verdadeiro. 
      *      
+     *  Tempo necessário para solução: 1h
+     * 
+     *  Big O Espaço: O(n)
+     *  Big O Tempo: O(n)
+     * 
      *  
-     *  Big O Espaço: 0(n)
-     *  Big O Tempo: 0(n)
-     * 
-     *  Precisei pesquisar sobre estrutura do HashMap no JS: https://medium.com/@lelianto.eko/hashmap-in-ja-3a5c03a8febc
+     *  Precisei pesquisar sobre definição de objeto: https://www.w3schools.com/js/js_object_definition.asp
      * 
      * 
      * 
-     * 
-     * @param {string} s /* palavra passada
-     * @param {string} t /* palavra pode ser ou não um anagrama de s 
-     * @return {boolean}
+     * @param {string} s primeira palavra passada para comparação do anagrama
+     * @param {string} t segunda palavra passada para comparação do anagrama 
+     * @return {boolean} retorna verdadeiro caso seja anagrama. Falso caso não
      */
     isAnagram(s, t) {
         if(s.length !== t.length){
@@ -36,10 +37,11 @@ class Solution {
         const countS = {};
         const countT = {};
         
-        for (let i = 0; i < s.length ; ++i){
+        for (let i = 0; i < s.length; ++i){
             countS[s[i]] = 1 + (countS[s[i]] || 0);
             countT[t[i]] = 1 + (countT[t[i]] || 0);
         }
+
         for(const key in countS){
             if(countS[key] !== countT[key]) {
                 return false;
